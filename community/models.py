a39b6b5c8875dcc0d34ed6c87e7f13154080037c
Kasparov <ianjkiplagat@gmail.com>
2021-06-03 18:06:48 +0300

    add user business

--- community/models.py
+++ community/models.py
@@ -37,4 +37,29 @@ class User(models.Model):
       self.save()
       
     def delete_user(self):
-      self.delete()       
+      self.delete()    
+      
+      
+class Business(models.Model):
+    name=models.CharField(max_length=50)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    email=models.EmailField()
+    neighborhood=models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+
+    def __str__(self):
+      return self.name
+
+    def save_business(self):
+      self.save()
+
+    def delete_business(self):
+      self.delete()
+        
+    @classmethod
+    def find_business(cls, name):
+      return cls.objects.filter(name_icontains=name) 
+    
+    @classmethod
+    def update_business(cls, id, name):
+      update = cls.objects.filter(id=id).update(name=name)
+      return update         
