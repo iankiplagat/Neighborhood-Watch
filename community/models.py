a1ed25c549c407f8ca069a836ec8e3fbb96da48e
Kasparov <ianjkiplagat@gmail.com>
2021-06-03 18:08:53 +0300

    run initial migrations

--- community/models.py
+++ community/models.py
@@ -44,7 +44,7 @@ class Business(models.Model):
     name=models.CharField(max_length=50)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     email=models.EmailField()
-    neighborhood=models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+    neighborhood=models.ForeignKey(Neighborhood,on_delete=models.CASCADE)
 
     def __str__(self):
       return self.name
