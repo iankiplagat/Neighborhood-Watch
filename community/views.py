cae6e8824f0a7db207d0e953ef03ad2debba4f96
Kasparov <ianjkiplagat@gmail.com>
2021-06-03 18:28:57 +0300

    add get and post requests for class user

--- community/views.py
+++ community/views.py
@@ -33,3 +33,18 @@ class BusinessList(APIView):
             serializers.save()
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)       
+      
+      
+class UserList(APIView):
+  def get(self, request, format=None):
+    users = User.objects.all()
+    serializers = UserSerializer(users, many=True)
+    return Response(serializers.data)  
+  
+  def post(self, request, format=None):
+        serializers = UserSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)  
+      
