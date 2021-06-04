1cf08bccd0753bcc431b85824fd8c03e8d8baaa0
Kasparov <ianjkiplagat@gmail.com>
2021-06-04 16:16:57 +0300

    add view function for posts

--- community/urls.py
+++ community/urls.py
@@ -9,20 +9,20 @@ urlpatterns=[
   path('api/business/',views.BusinessList.as_view(),name='business'),
   path('api/users/',views.UserList.as_view(),name='users'),
   path('api/profile/',views.ProfileList.as_view(),name='profiles'),
-  # path('api/post/',views.PostList.as_view(),name='post'),
+  path('api/post/',views.PostList.as_view(),name='post'),
   
   #update urls
   path('api/update/profile/<int:pk>/',views.ProfileList.as_view(),name='update_profile'),
   path('api/update/users/<int:pk>/',views.UserList.as_view(),name='update_users'),
   re_path('api/update/business/(?P<pk>[0-9]+)/',views.BusinessList.as_view(),name='update_business'),
   path('api/update/hood/<int:pk>/',views.NeighborhoodList.as_view(),name='update_neighbors'),
-  # path('api/update/post/<int:pk>/',views.PostList.as_view(),name='update_post'),
+  path('api/update/post/<int:pk>/',views.PostList.as_view(),name='update_post'),
   
   #delete urls
   path('api/delete/users/<int:pk>/',views.UserList.as_view(),name='delete_users'),
   re_path('api/delete/hood/(?P<pk>[0-9]+)/',views.NeighborhoodList.as_view(),name='delete_neighbors'),
   path('api/delete/business/<int:pk>/',views.BusinessList.as_view(),name='delete_business'),
-  # path('api/delete/post/<int:pk>/',views.PostList.as_view(),name='delete_post')
+  path('api/delete/post/<int:pk>/',views.PostList.as_view(),name='delete_post')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- community/views.py
+++ community/views.py
@@ -187,4 +187,56 @@ class ProfileList(APIView):
       return Response(response, status=status.HTTP_200_OK)
     else:
       return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+class PostList(APIView):
+  serializer_class = PostSerializer
+  def get_post(self, pk):
+    try:
+        return Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        raise Http404()
+
+  def get(self, request, format=None):
+    post = Post.objects.all()
+    serializers = self.serializer_class(post, many=True)
+    return Response(serializers.data)
+
+  def post(self, request, format=None):
+    serializers = self.serializer_class(data=request.data)
+    if serializers.is_valid():
+      serializers.save()
+
+      post = serializers.data
+      response = {
+          'data': {
+              'post': dict(post),
+              'status': 'success',
+              'message': 'post created successfully',
+          }
+      }
+      return Response(response, status=status.HTTP_200_OK)
+    return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def put(self, request, pk, format=None):
+    post = self.get_post(pk)
+    serializers = self.serializer_class(post, request.data)
+    if serializers.is_valid():
+      serializers.save()
+      post_data = serializers.data
+      response = {
+          'data': {
+              'post': dict(post_data),
+              'status': 'success',
+              'message': 'post updated successfully',
+          }
+      }
+      return Response(response, status=status.HTTP_200_OK)
+    else:
+      return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, pk, format=None):
+    post = self.get_post(pk)
+    post.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)    
   
