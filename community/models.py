82f39a5104e4e1e7835c30d9025b51af776aabe1
Kasparov <ianjkiplagat@gmail.com>
2021-06-04 16:13:11 +0300

    add post model

--- community/models.py
+++ community/models.py
@@ -90,4 +90,15 @@ class Profile(models.Model):
     self.save()
 
   def delete_user(self):
-    self.delete()    
+    self.delete() 
+    
+    
+class Post(models.Model):
+  post_name=models.CharField(max_length=100)
+  post_content=models.TextField()
+  pub_date=models.DateTimeField(auto_now_add=True)
+  profile=models.ForeignKey(Profile,on_delete=models.CASCADE, related_name='post',null=True)
+  hood=models.ForeignKey(Neighborhood,on_delete=models.CASCADE,related_name='post_hood',null=True)
+
+  def __str__(self):
+    return self.post_name       
